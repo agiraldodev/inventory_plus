6461948a59061a994cd7aacf15a4a1308ed9086c
Andres Giraldo <agiraldodev@gmail.com>
2024-06-26 23:23:47 -0500

    Registro del admin

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Equipo, Mantenimiento
 
-# Register your models here.
+admin.site.site_header = 'Control de Mantenimientos de Equipos'
+admin.site.index_title = 'InventoryPlus | Panel administrativo'
+
+admin.site.register(Equipo)
+admin.site.register(Mantenimiento)
